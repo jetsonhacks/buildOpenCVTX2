0be094f0582d23042d28cc9bc8ce7982133900f7
jetsonhacks <jim@jetsonhacks.com>
2017-04-05 08:31:57 -0700

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # buildOpenCVTX2
 Build and install OpenCV for the NVIDIA Jetson TX2
+
+These scripts build OpenCV version 3.2 for the Jetson TX2.
+
+JetPack gives the option of installing OpenCV4Tegra (OpenCV 2.4) with accelerated CPU functions. There are several common packages that have issue with this installation, so here's a recipe for building a OpenCV from source.
+
+OpenCV is a rich environment which can be configured in many different ways. You should configure OpenCV for your needs, by modifying the build file "buildOpenCV.sh". Note that selecting different options in OpenCV may also have additional library requirements which are not included in these scripts.
+
+To run the the build file
+
+$ ./buildOpenCV.sh
+
+The build system has been known at times to have issues. It's worth doing a sanity check after the build is complete:
+
+$ cd $HOME/opencv/build
+$ make
+
+This should ensure that everything has been built.
+
+After this, you can install the new build:
+
+$ cd $HOME/opencv/build
+$ sudo make install
+
+Notes:
+There are issues if have both OpenCV4Tegra and a regular OpenCV build installed at the same time. Most people do not install OpenCV4Tegra on their machine if using the OpenCV build.
+
+The Jetson is an aarch64 machine, which means that the OpenCV configuration variable ENABLE_NEON is ignored. The compiler includes NEON support for all machines with aarch64 architecture.
+
+
+ 
---
+++ buildOpenCV.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+cd $HOME
+sudo apt-get install -y \
+    libglew-dev \
+    libtiff5-dev \
+    zlib1g-dev \
+    libjpeg-dev \
+    libpng12-dev \
+    libjasper-dev \
+    libavcodec-dev \
+    libavformat-dev \
+    libavutil-dev \
+    libpostproc-dev \
+    libswscale-dev \
+    libeigen3-dev \
+    libtbb-dev \
+    libgtk2.0-dev \
+    cmake \
+    pkg-config
+
+# Python 2.7
+sudo apt-get install -y python-dev python-numpy python-py python-pytest -y
+
+# GStreamer
+# sudo apt-get install libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev -y
+
+# OpenGL
+# sudo apt-get install qtbase5-dev libgtkglext1 libgtkglext1-dev -y
+
+# V4L
+# sudo apt-get install -y libv4l-dev v4l-utils qv4l2 v4l2ucp
+
+git clone https://github.com/opencv/opencv.git
+cd opencv
+git checkout -b v3.2.0 3.2.0
+git clone https://github.com/opencv/opencv_extra.git
+cd opencv_extra
+git checkout -b v3.2.0 3.2.0
+
+cd $HOME/opencv
+mkdir build
+cd build
+# Jetson TX2 
+cmake \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_PREFIX=/usr \
+    -DBUILD_PNG=OFF \
+    -DBUILD_TIFF=OFF \
+    -DBUILD_TBB=OFF \
+    -DBUILD_JPEG=OFF \
+    -DBUILD_JASPER=OFF \
+    -DBUILD_ZLIB=OFF \
+    -DBUILD_EXAMPLES=ON \
+    -DBUILD_opencv_java=OFF \
+    -DBUILD_opencv_python2=ON \
+    -DBUILD_opencv_python3=OFF \
+    -DENABLE_PRECOMPILED_HEADERS=OFF \
+    -DWITH_OPENCL=OFF \
+    -DWITH_OPENMP=OFF \
+    -DWITH_FFMPEG=ON \
+    -DWITH_GSTREAMER=OFF \
+    -DWITH_GSTREAMER_0_10=OFF \
+    -DWITH_CUDA=ON \
+    -DWITH_GTK=ON \
+    -DWITH_VTK=OFF \
+    -DWITH_TBB=ON \
+    -DWITH_1394=OFF \
+    -DWITH_OPENEXR=OFF \
+    -DCUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda-8.0 \
+    -DCUDA_ARCH_BIN=6.2 \
+    -DCUDA_ARCH_PTX="" \
+    -DINSTALL_C_EXAMPLES=ON \
+    -DINSTALL_TESTS=ON \
+    -DOPENCV_TEST_DATA_PATH=../opencv_extra/testdata \
+    ../
+
+# make -j6
