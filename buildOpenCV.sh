35904b0d14736dbd754e75fd8afef3f7cd81d65d
jetsonhacks <jim@jetsonhacks>
2018-05-10 08:48:58 -0700

    Better build

--- buildOpenCV.sh
+++ buildOpenCV.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 # License: MIT. See license file in root directory
-# Copyright(c) JetsonHacks (2017)
+# Copyright(c) JetsonHacks (2017-2018)
+
+INSTALL_PYTHON2=ON
+INSTALL_PYTHON3=ON
+
+
 cd $HOME
 sudo apt-get install -y \
     libglew-dev \
@@ -22,6 +27,9 @@ sudo apt-get install -y \
 
 # Python 2.7
 sudo apt-get install -y python-dev python-numpy python-py python-pytest -y
+
+
+
 # GStreamer support
 sudo apt-get install -y libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev 
 
@@ -39,37 +47,39 @@ mkdir build
 cd build
 # Jetson TX2 
 cmake \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_INSTALL_PREFIX=/usr \
-    -DBUILD_PNG=OFF \
-    -DBUILD_TIFF=OFF \
-    -DBUILD_TBB=OFF \
-    -DBUILD_JPEG=OFF \
-    -DBUILD_JASPER=OFF \
-    -DBUILD_ZLIB=OFF \
-    -DBUILD_EXAMPLES=ON \
-    -DBUILD_opencv_java=OFF \
-    -DBUILD_opencv_python2=ON \
-    -DBUILD_opencv_python3=OFF \
-    -DENABLE_PRECOMPILED_HEADERS=OFF \
-    -DWITH_OPENCL=OFF \
-    -DWITH_OPENMP=OFF \
-    -DWITH_FFMPEG=ON \
-    -DWITH_GSTREAMER=ON \
-    -DWITH_GSTREAMER_0_10=OFF \
-    -DWITH_CUDA=ON \
-    -DWITH_GTK=ON \
-    -DWITH_VTK=OFF \
-    -DWITH_TBB=ON \
-    -DWITH_1394=OFF \
-    -DWITH_OPENEXR=OFF \
-    -DCUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda-8.0 \
-    -DCUDA_ARCH_BIN=6.2 \
-    -DCUDA_ARCH_PTX="" \
-    -DINSTALL_C_EXAMPLES=ON \
-    -DINSTALL_TESTS=ON \
-    -DOPENCV_TEST_DATA_PATH=../opencv_extra/testdata \
+    -D CMAKE_BUILD_TYPE=Release \
+    -D CMAKE_INSTALL_PREFIX=/usr \
+    -D BUILD_PNG=OFF \
+    -D BUILD_TIFF=OFF \
+    -D BUILD_TBB=OFF \
+    -D BUILD_JPEG=OFF \
+    -D BUILD_JASPER=OFF \
+    -D BUILD_ZLIB=OFF \
+    -D BUILD_EXAMPLES=ON \
+    -D BUILD_opencv_java=OFF \
+    -D BUILD_opencv_python2=ON \
+    -D BUILD_opencv_python3=OFF \
+    -D ENABLE_PRECOMPILED_HEADERS=OFF \
+    -D WITH_OPENCL=OFF \
+    -D WITH_OPENMP=OFF \
+    -D WITH_FFMPEG=ON \
+    -D WITH_GSTREAMER=ON \
+    -D WITH_GSTREAMER_0_10=OFF \
+    -D WITH_CUDA=ON \
+    -D WITH_GTK=ON \
+    -D WITH_VTK=OFF \
+    -D WITH_TBB=ON \
+    -D WITH_1394=OFF \
+    -D WITH_OPENEXR=OFF \
+    -D CUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda-8.0 \
+    -D CUDA_ARCH_BIN=6.2 \
+    -D CUDA_ARCH_PTX="" \
+    -D INSTALL_C_EXAMPLES=ON \
+    -D INSTALL_TESTS=ON \
+    -D OPENCV_TEST_DATA_PATH=../opencv_extra/testdata \
     ../
 
 # Consider using all 6 cores; $ sudo nvpmodel -m 2 or $ sudo nvpmodel -m 0
-make -j4
+NUM_CPU=$(nproc)
+make -j$(($NUM_CPU - 1))
+sudo make install
